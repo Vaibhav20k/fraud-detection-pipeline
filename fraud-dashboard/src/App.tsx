85c83476d3e0d0fb1f1ba81675c102a59816e4e2
clone891 <mrclone136@gmail.com>
2026-07-24 02:50:35 +0530

    feat: register all routes in App.tsx (Dashboard, Transactions, RiskQueue, Watchlists, Reports, NewInvestigation)

--- fraud-dashboard/src/App.tsx
+++ fraud-dashboard/src/App.tsx
@@ -2,6 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import AppLayout from "./components/layout/AppLayout";
 import Dashboard from "./pages/Dashboard";
+import Transactions from "./pages/Transactions";
+import RiskQueue from "./pages/RiskQueue";
+import Watchlists from "./pages/Watchlists";
+import Reports from "./pages/Reports";
+import NewInvestigation from "./pages/NewInvestigation";
 
 function App() {
   return (
@@ -9,6 +14,11 @@ function App() {
       <Routes>
         <Route element={<AppLayout />}>
           <Route path="/" element={<Dashboard />} />
+          <Route path="/transactions" element={<Transactions />} />
+          <Route path="/risk-queue" element={<RiskQueue />} />
+          <Route path="/watchlists" element={<Watchlists />} />
+          <Route path="/reports" element={<Reports />} />
+          <Route path="/investigation/new" element={<NewInvestigation />} />
         </Route>
       </Routes>
     </BrowserRouter>
